7552ee1b8e131ff41a3a885d73b3b1e6389ae28a
Kevin <kjrodgers26@gmail.com>
2024-09-12 14:49:34 -0400

    added get_cost() method to Wall classs

--- OOP/Ch 1 - Classes/7 - Classes/Wall.py
+++ OOP/Ch 1 - Classes/7 - Classes/Wall.py
@@ -1,13 +1,10 @@
 class Wall:
     armor = 10
     height = 5
-    
+
     def fortify(self):
         self.armor *= 2
 
-black_wall = Wall()
-
-print(black_wall.armor)
-black_wall.fortify()
-print(black_wall.armor)
-
+    def get_cost(self):
+        cost = self.armor * self.height
+        return cost
