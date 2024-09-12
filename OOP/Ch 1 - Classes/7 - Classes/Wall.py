7d1c2fae1da4a1d14c208693457d31c8ed0cc61b
Kevin <kjrodgers26@gmail.com>
2024-09-12 14:39:34 -0400

    added fortify method

--- OOP/Ch 1 - Classes/7 - Classes/Wall.py
+++ OOP/Ch 1 - Classes/7 - Classes/Wall.py
@@ -1,4 +1,13 @@
 class Wall:
     armor = 10
     height = 5
+    
+    def fortify(self):
+        self.armor *= 2
+
+black_wall = Wall()
+
+print(black_wall.armor)
+black_wall.fortify()
+print(black_wall.armor)
 
