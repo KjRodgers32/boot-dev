2e1379e5a47e666268df6ed0d305c598d30ffa17
Kevin <kjrodgers26@gmail.com>
2024-09-12 15:01:30 -0400

    added constructor to wall class

--- OOP/Ch 1 - Classes/7 - Classes/Wall.py
+++ OOP/Ch 1 - Classes/7 - Classes/Wall.py
@@ -1,10 +1,6 @@
 class Wall:
-    armor = 10
-    height = 5
-
-    def fortify(self):
-        self.armor *= 2
-
-    def get_cost(self):
-        cost = self.armor * self.height
-        return cost
+    def __init__(self, depth, height, width):
+        self.depth = depth
+        self.height = height
+        self.width = width
+        self.volume = depth * height * width
