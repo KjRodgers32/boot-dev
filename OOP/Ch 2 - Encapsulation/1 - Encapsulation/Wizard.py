5e53813a46cfc1a81f4e7225a59c0d38fccbd70a
KjRodgers32 <kjrodgers26@gmail.com>
2024-09-17 20:33:39 -0400

    added cast_fireball and __is_alive methods to Wizard class

--- OOP/Ch 2 - Encapsulation/1 - Encapsulation/Wizard.py
+++ OOP/Ch 2 - Encapsulation/1 - Encapsulation/Wizard.py
@@ -7,7 +7,17 @@ class Wizard:
         self.health = self.__stamina * 100
 
     def get_fireballed(self):
-            self.health -= 500
+        self.health -= 500
         
     def drink_mana_potion(self):
-            self.mana += 100
+        self.mana += 100
+    
+    def cast_fireball(self, target):
+        if self.mana >= 50:
+            self.mana -= 50
+            target.get_fireballed()
+        else:
+            raise Exception(f"{self.name} cannot cast fireball!")
+    
+    def __is_alive(self):
+        return self.health > 0
