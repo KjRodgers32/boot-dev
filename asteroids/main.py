6c4d32671a1d20038e43756ab98f917f1f74dbef
KjRodgers32 <kjrodgers26@gmail.com>
2024-09-22 19:00:12 -0400

    added player along with player rotation to the left and right

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,14 +1,24 @@
 import pygame
 from constants import *
+from Player import Player 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        screen.fill("#FFFFCC")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()
+        dt = clock.tick() / 1000
+
 if __name__ == "__main__":
     main()
