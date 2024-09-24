2deb554425d84d36a0ead83a698705e8d95ac749
KjRodgers32 <kjrodgers26@gmail.com>
2024-09-24 06:40:57 -0400

    added functional programming directory with first module code and test

---
+++ Functional Programming/1 - Functional Programming/main.py
@@ -0,0 +1,13 @@
+def stylize_title(document):
+    return add_border(center_title(document))
+
+def center_title(document):
+    width = 40
+    title = document.split("\n")[0]
+    centered_title = title.center(width)
+    return document.replace(title, centered_title)
+
+def add_border(document):
+    title = document.split("\n")[0]
+    border = "*" * len(title)
+    return document.replace(title, title + "\n" + border)
