c497edfeb546378511eca24c36e8e46c5bbb99d5
KjRodgers32 <kjrodgers26@gmail.com>
2024-09-26 08:51:36 -0400

    added recursive tree module with code and tests

--- Functional Programming/Ch 4 - Recursion/12 - Recursion on a Tree/main.py
+++ Functional Programming/Ch 4 - Recursion/12 - Recursion on a Tree/main.py
@@ -1,9 +1,9 @@
 def list_files(current_filetree, current_path=""):
-    list_of_paths = []
-    for key in current_filetree:
-        current_path = f"/{key}"
-        if current_filetree[key]:
-           list_files(current_filetree[key], current_path)
-        else:
-           list_of_paths.append(current_path)
-    return list_of_paths
+   list_of_paths = []
+   for key in current_filetree:
+      key_to_append = f"/{key}"
+      if isinstance(current_filetree[key], type(None)):
+         list_of_paths.append(current_path + key_to_append)
+      else:
+         list_of_paths.extend(list_files(current_filetree[key], current_path + key_to_append))
+   return list_of_paths
