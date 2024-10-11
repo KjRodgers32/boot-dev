4aa0152b248efadd5242136d937fbece9afea2f2
KjRodgers32 <kjrodgers26@gmail.com>
2024-10-11 13:41:42 -0400

    added representation for constant vairables

--- Learning Go/Ch 1 - Variables/2 - Basic Variable/main.go
+++ Learning Go/Ch 1 - Variables/2 - Basic Variable/main.go
@@ -8,6 +8,7 @@ func main() {
 	var costPerSms float64 = .85
 	var hasPermission bool = true
 	var username string = "Kevin Rodgers"
+	const middlename = "Lamont" // constant
 
 	// Common way
 	smsSendingLimitTwo := 200
@@ -15,6 +16,6 @@ func main() {
 	hasPermissionTwo := true
 	usernameTwo := "Kevin Rodgers"
 
-	fmt.Printf("%d %.2f %v %s\n", smsSendingLimit, costPerSms, hasPermission, username)
-	fmt.Printf("%d %.2f %v %s", smsSendingLimitTwo, costPerSmsTwo, hasPermissionTwo, usernameTwo)
+	fmt.Printf("%d %.2f %v %s %s\n", smsSendingLimit, costPerSms, hasPermission, username, middlename)
+	fmt.Printf("%d %.2f %v %s %s", smsSendingLimitTwo, costPerSmsTwo, hasPermissionTwo, usernameTwo, middlename)
 }
