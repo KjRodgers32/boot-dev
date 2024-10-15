f274c1603824987131e04908efe384a33456ad78
Kevin <kjrodgers26@gmail.com>
2024-10-14 22:23:59 -0400

    added errors module with code and test

---
+++ Learning Go/Ch 6 - Errors/1 - Error Interface/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
+	customerCost, err := sendSMS(msgToCustomer)
+	if err != nil {
+		return 0, err
+	}
+
+	spouseCost, err := sendSMS(msgToSpouse)
+	if err != nil {
+		return 0, err
+	}
+
+	totalCost := customerCost + spouseCost
+	return totalCost, nil
+
+}
+
+func sendSMS(message string) (int, error) {
+	const maxTextLen = 25
+	const costPerChar = 2
+	if len(message) > maxTextLen {
+		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
+	}
+	return costPerChar * len(message), nil
+}
