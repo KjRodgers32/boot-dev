dc796273f8bb911c7f06c2cd115403ad76d2f5b7
Kevin <kjrodgers26@gmail.com>
2024-10-16 22:02:23 -0400

    added loops module with test and code

--- Learning Go/Ch 7 - Loops/1 - Loops/main.go
+++ Learning Go/Ch 7 - Loops/1 - Loops/main.go
@@ -1,10 +1,13 @@
 package main
 
-func bulkSend(numMessages int) float64 {
-	messageCost := 0
-	for i := 0; i < numMessages; i++ {
-		messageCost += 1 + i/100
-	}
+import "fmt"
 
+func bulkSend(numMessages int) float64 {
+	var messageCost float64
+	for i := 0; i < numMessages; i++ {
+		fee := float64(i) / 100.0
+		fmt.Println(fee)
+		messageCost += 1 + fee
+	}
 	return float64(messageCost)
 }
